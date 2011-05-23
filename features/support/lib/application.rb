5cc60c1495164c9fdd31eb4c6377964310722495
Jacob Sin <jacob.sin@gmail.com>
2011-05-24 02:28:42 +0800

    made rakefile copy resource to the correct target dir

--- features/support/lib/application.rb
+++ features/support/lib/application.rb
@@ -7,17 +7,9 @@ class Application
   attr_reader :port
 
   def initialize
+    rjb_load
     @port = 3333
-    jars = Dir.glob(File.join('m2', '**', '*.jar')).select{|f| f !~ /sources\.jar/ }
-    classpath = "target/resources:target/java:#{jars.join(':')}"
-    jvmargs = [
-        '-Duser.timezone=UTC',
-        '-Djava.util.logging.config.file=src/main/resources/logging.properties',
-        '-Dlogback.access.config.file=src/main/resources/logback-access.xml'
-    ]
-    Rjb::load(classpath, jvmargs)
-    run_jetty = Rjb::import('bluemount.web.RunJetty')
-    @jetty = run_jetty.new(@port)
+    @jetty = Rjb::import('bluemount.web.RunJetty').new(@port)
     @jetty.start false
   end
 
@@ -28,4 +20,19 @@ class Application
   def stop
     @jetty.stop
   end
+
+  private
+
+  def rjb_load
+    separator = Config::CONFIG['target_os'] =~ /windows/ ? ';' : ':'
+    jars = Dir.glob(File.join('m2', '**', '*.jar')).select{|f| f !~ /sources\.jar/ }
+    classpath = "target/test/java:target/java:#{jars.join(separator)}"
+    jvmargs = [
+        '-Duser.timezone=UTC',
+        '-Djava.util.logging.config.file=src/main/resources/logging.properties',
+        '-Dlogback.access.config.file=src/main/resources/logback-access.xml'
+    ]
+    Rjb::load(classpath, jvmargs)
+  end
+
 end
