83fa576592a38fcffb37ab1615824993bcd0c9ef
Jacob Sin <jacob.sin@gmail.com>
2012-06-22 01:35:37 +0800

    Upgraded jars

--- rake/artifacts.rb
+++ rake/artifacts.rb
@@ -1,15 +1,14 @@
 ARTIFACTS = {
     :shipped => %w{
-  org.eclipse.jetty.aggregate jetty-webapp 7.3.0.v20110203
-  commons-io commons-io 2.0.1
-  org.restlet.jee org.restlet 2.1-M4
-  org.restlet.jee org.restlet.ext.servlet 2.1-M4
-  org.restlet.jee org.restlet.ext.jackson 2.1-M4
-  org.restlet.jee org.restlet.ext.spring 2.1-M4
+  org.eclipse.jetty.aggregate jetty-webapp 7.6.0.v20120127
+  commons-io commons-io 2.3
+  org.restlet.jee org.restlet 2.1-RC5
+  org.restlet.jee org.restlet.ext.servlet 2.1-RC5
+  org.restlet.jee org.restlet.ext.jackson 2.1-RC5
+  org.restlet.jee org.restlet.ext.spring 2.1-RC5
   com.google.inject guice 3.0
   com.google.inject.extensions guice-servlet 3.0
   com.google.inject.extensions guice-multibindings 3.0
-  javax.inject javax.inject 1
   de.devsurf.injection.guice de.devsurf.injection.guice.core 0.8.7
   de.devsurf.injection.guice.scanner de.devsurf.injection.guice.scanner.asm 0.8.7
   org.codehaus.groovy groovy-all 1.8.6
@@ -19,9 +18,9 @@ ARTIFACTS = {
   ch.qos.logback logback-access 0.9.27
 },
     :reference => %w{
-  junit junit 4.7
-  org.apache.ant ant-junit 1.7.1
-  org.mockito mockito-core 1.8.5
+  junit junit 4.10
+  org.apache.ant ant-junit 1.8.4
+  org.mockito mockito-core 1.9.0
   org.jruby jruby-complete 1.6.7.2
 }
 }
