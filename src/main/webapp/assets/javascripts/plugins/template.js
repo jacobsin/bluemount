2c8ebaa6ac754783c129d45842b26e0c9d012e1c
Jacob Sin <jacob.sin@gmail.com>
2012-08-16 01:35:41 +0800

    add more memory for rjs:optimise task add build optimisation handling for template plugin

--- src/main/webapp/assets/javascripts/plugins/template.js
+++ src/main/webapp/assets/javascripts/plugins/template.js
@@ -1,6 +1,10 @@
 define({
-  load: function (name, req, load, config) {
-    req(['templates/templates', 'text!templates/'+name + '.underscore'], function (Templates, value) {
+  load:function (name, req, load, config) {
+    req(['templates/templates', 'text!templates/' + name + '.underscore'], function (Templates, value) {
+      if (config.isBuild && !config.inlineText) {
+        load();
+        return;
+      }
       Templates.add(name, value);
       load(Templates.get(name));
     });
