5174ff7b503ddf4f660dfc3168dea379b3bf59e0
Jacob Sin <jacob.sin@gmail.com>
2012-08-16 03:26:39 +0800

    tweak the template plugin and optimiser build

--- src/main/webapp/assets/javascripts/plugins/template.js
+++ src/main/webapp/assets/javascripts/plugins/template.js
@@ -1,7 +1,7 @@
 define({
   load:function (name, req, load, config) {
     req(['templates/templates', 'text!templates/' + name + '.underscore'], function (Templates, value) {
-      if (config.isBuild && !config.inlineText) {
+      if (config.isBuild) {
         load();
         return;
       }
